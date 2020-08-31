6e47731dc1c13b4be0d6f93a6ba87c7edf91e895
Luis Gustavo <lluisgusttavo@gmail.com>
2020-08-31 07:20:01 -0300

    Already looking like a job app

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+import useFetchJobs from './userFetchJobs'
+import { Container } from 'react-bootstrap'
+import Job from './Job'
+import JobsPagination from './JobsPagination';
+
+function App() {
+  const [params, setParams] = useState({})
+  const [page, setPage] = useState(1)
+  const {jobs, loading, error} = useFetchJobs(params, page);
+  
+  console.log(page)
+  return (
+    <Container className="my-4">
+      <h1 className="mb-4">GitHub Jobs</h1>
+      <JobsPagination page={page} setPage={setPage} />
+      {loading && <h1>Loading...</h1>}
+      {error && <h1>Error. Try Refreshing.</h1>}
+      {jobs.map(job => {
+        return <Job key={job.id} job={job} />
+      })}
+      <JobsPagination page={page} setPage={setPage} />
+    </Container>
+  )
+}
+
+export default App;
